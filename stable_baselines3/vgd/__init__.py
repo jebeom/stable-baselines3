1797b61680e68877afa17ca7f5123b327ce25fc6
jebeom <cowpqja3302@naver.com>
2025-12-13 17:41:26 +0900

    vgd_dynamic

--- stable_baselines3/vgd/__init__.py
+++ stable_baselines3/vgd/__init__.py
@@ -1,6 +1,6 @@
 from stable_baselines3.sac.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
 from stable_baselines3.vgd.vgd import VGD
-
-__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "VGD"]
+from stable_baselines3.vgd.vgd_dyna import VGD_DYNA
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "VGD", "VGD_DYNA"]
 
 
